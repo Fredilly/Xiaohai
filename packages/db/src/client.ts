1a823fad7bc036b20b7a6b3807484de39dba7df4
lixia67 <lazurite2023@163.com>
2026-09-15 14:23:17 +0800

    feat: wire M5 commerce migration

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -3,8 +3,9 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import pg from 'pg';
 import * as coreSchema from './schema.js';
 import * as cmsSchema from './cms-schema.js';
+import * as commerceSchema from './commerce-schema.js';
 
-const schema = { ...coreSchema, ...cmsSchema };
+const schema = { ...coreSchema, ...cmsSchema, ...commerceSchema };
 
 export function createDatabase(env: NodeJS.ProcessEnv = process.env) {
   const config = loadDatabaseConfig(env);
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,6 @@
 export { createDatabase } from './client.js';
 export { cmsPages, cmsSections } from './cms-schema.js';
+export { books, bookEditions, products, skus, productMedia, carts, cartItems, userAddresses, orders, orderItems } from './commerce-schema.js';
 export {
   consumerUsers,
   migrationBatches,
